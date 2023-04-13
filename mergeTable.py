21ddaed688fce9db3010a55d99696dd8c04d25a6
ShankarLayek2008 <shankarlayek2008@gmail.com>
2023-04-13 22:27:31 +0530

    Updating mergeTable.py file

--- mergeTable.py
+++ mergeTable.py
@@ -1,10 +1,8 @@
 import csv
 
-import pandas as pd
 from csv import writer
 
 with open("Table1.csv", "r+") as T1:
-#     # t = T1.read()
     table1 = csv.reader(T1)
 
     with open("Table2.csv", "r+") as T2:
@@ -13,17 +11,17 @@ with open("Table1.csv", "r+") as T1:
         with open("Table3.csv", "r+") as T3:
             table3 = csv.reader(T3)
 
-            with open("combineTable.csv", "a") as Tmerge:
+            with open("combineTable.csv", "a", newline='') as Tmerge:
                 Tmerge.writelines("START OF DOC")
                 Tmerge.writelines('\n')
                 Tmerge.writelines("Table1")
                 Tmerge.writelines('\n')
 
                 writer_object = writer(Tmerge)
-                for column in table1:
-                    writer_object.writerow(column)
+                for table1_row in table1:
+                    writer_object.writerow(table1_row)
 
-                # Tmerge.writelines('\n')
+                Tmerge.writelines('\n')
                 Tmerge.writelines("Evaluate")
                 Tmerge.writelines('\n')
                 Tmerge.writelines('\n')
@@ -32,25 +30,33 @@ with open("Table1.csv", "r+") as T1:
                 Tmerge.writelines('\n')
 
                 writer_object2 = writer(Tmerge)
-                for col2 in table2:
-                    writer_object2.writerow(col2)
+                for table2_row in table2:
+                    writer_object2.writerow(table2_row)
 
-                # Tmerge.writelines('\n')
+                Tmerge.writelines('\n')
                 Tmerge.writelines("Cred=Oauth")
                 Tmerge.writelines('\n')
-                # Tmerge.writelines('\n')
-                # Tmerge.writelines('\n')
                 Tmerge.writelines("ID=202365")
                 Tmerge.writelines('\n')
                 Tmerge.writelines('\n')
 
                 writer_object3 = writer(Tmerge)
-                for col3 in table3:
-                    writer_object2.writerow(col3)
+                for table3_row in table3:
+                    writer_object2.writerow(table3_row)
 
+                Tmerge.writelines('\n')
                 Tmerge.writelines('\n')
                 Tmerge.writelines("upload=True")
                 Tmerge.writelines('\n')
                 Tmerge.writelines("END")
-
                 Tmerge.close()
+        T3.close()
+    T2.close()
+T1.close()
+
+
+
+
+
+
+
